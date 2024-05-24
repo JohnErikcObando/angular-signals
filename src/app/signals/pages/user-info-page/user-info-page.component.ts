32644fc4564f2cf749a8bc87113d5447f4097a92
John <jeobando11@hotmail.com>
2024-05-24 17:22:26 -0500

    signal

---
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -0,0 +1,44 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { single } from 'rxjs';
+import { User } from '../../interfaces/userRequest';
+
+@Component({
+  selector: 'app-user-info-page',
+  templateUrl: './user-info-page.component.html',
+  styleUrls: ['./user-info-page.component.css'],
+})
+export class UserInfoPageComponent {
+  private usersService = inject(UserService);
+  public userId = signal(1);
+
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+  public fullName = computed<string>(() => {
+    if (!this.currentUser()) return 'Usuario no encontrado';
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number) {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById(id).subscribe({
+      next: (user) => {
+        this.currentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      error: () => {
+        this.userWasFound.set(false);
+        this.currentUser.set(undefined);
+      },
+    });
+  }
+}
